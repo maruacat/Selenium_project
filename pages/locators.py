ce5666fe7a6f76d5a3fe172903ba19afc62a1b33
admin <admin@admins-MacBook-Air.local>
2022-09-22 12:28:20 +0300

    login page add

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,13 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, 'form#login_form')
+    LOGIN_FORM_EMAIL = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_FORM_PASSWORD = (By.CSS_SELECTOR, '#id_login-password')
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '#login_form button.btn')
+
+    REGISTER_FORM = (By.CSS_SELECTOR, 'form#register_form')
+    REGISTER_FORM_EMAIL = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_FORM_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '#register_form button.btn')
--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,8 @@
 from .pages.main_page import MainPage
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
+from .pages.base_page import BasePage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -8,4 +10,10 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_form()
+    login_page.should_be_register_form()
+    login_page.should_be_login_url()
+
+    
 
