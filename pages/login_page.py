5bdc580d5d22ecb6ffdc1dab0ddd71c47825ff72
Марина Кириллова <marinakirillova@MacBook-Air-Marina.local>
2022-10-03 17:02:03 +0500

    Change page

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,6 +14,5 @@ class LoginPage(BasePage):
     def should_be_login_form(self):
          assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Login form is not present'
        
-
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Register form is not present'
--- test_main_page.py
+++ test_main_page.py
@@ -3,17 +3,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.login_page import LoginPage
 from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
-    login_page = LoginPage(browser, link)
-    login_page.should_be_login_form()
-    login_page.should_be_register_form()
-    login_page.should_be_login_url()
-
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     
 
